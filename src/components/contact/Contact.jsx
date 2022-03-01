81a2d20418d8a8e8b9cc5e516291007a7271b196
Jay Abella <Jenny.abella1@gmail.com>
2022-03-01 14:43:36 -0800

    Change Contact back to using Emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,69 @@
 import "./contact.scss";
-import { useState } from "react";
+import emailjs from '@emailjs/browser';
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import Swal from 'sweetalert2';
 
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_USER_ID;
 
 export default function Contact() {
-  const [submitted, setSubmitted] = useState(false);
-  
-  function handleSubmit (e) {
-    e.preventDefault();
-    setSubmitted(true);
-  }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+          .then((result) => {
+            console.log(result.text);
+            Swal.fire({
+              icon: 'success',
+              title: 'Message Sent Successfully'
+            })
+          }, (error) => {
+            console.log(error.text);
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops, something went wrong',
+              text: error.text,
+            })
+          });
+        e.target.reset()
+      };
     return(
         <div className="contact" id="contact">
             <h1>Contact</h1>
-            {submitted && <div class='success-message'>Thanks! I'll get back to you ASAP.</div>}
-            <form
-            name="contact-form"
-            method="post"
-            onSubmit={handleSubmit}
-            data-netlify="true"
-            data-netlify-honeypot="bot-field">
-              <input type="hidden" name="form-name" value="contact-form" />
-              <input type="text" placeholder="Enter your name" name="name" />
-              <input type="text" placeholder="Enter your email" name="email" />
-              <textarea placeholder="Type your message here" name="message" />
-              <button type="submit">Submit</button>
-            </form>
+        <Form onSubmit={handleOnSubmit}>
+                <Form.Field
+                className='form-label'
+                id='form-input-control-email'
+                control={Input}
+                label='Email'
+                name='from_email'
+                placeholder='Email'
+                required
+                icon='mail'
+                iconPosition='left'
+                />
+                <Form.Field
+                id='form-input-control-last-name'
+                className='form-label'
+                control={Input}
+                label='Name'
+                name='from_name'
+                placeholder='Name'
+                required
+                icon='user circle'
+                iconPosition='left'
+                />
+                <Form.Field
+                id='form-textarea-control-opinion'
+                className='form-label'
+                control={TextArea}
+                label='Message'
+                name='message'
+                placeholder='Message'
+                required
+                />
+                <Button type='submit' color='green'>Submit</Button>
+          </Form>
         </div>
     );
 }
