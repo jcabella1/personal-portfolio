33a68c71792cda5ce3ab15f524bb78b5dde4d2f1
Jay Abella <Jenny.abella1@gmail.com>
2022-02-28 19:36:13 -0800

    Change contact form to use netlify form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,69 +1,28 @@
 import "./contact.scss";
-import emailjs from '@emailjs/browser';
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
-import Swal from 'sweetalert2';
+import { useState } from "react";
 
-const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
-const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
-const USER_ID = process.env.REACT_APP_USER_ID;
 
 export default function Contact() {
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-          .then((result) => {
-            console.log(result.text);
-            Swal.fire({
-              icon: 'success',
-              title: 'Message Sent Successfully'
-            })
-          }, (error) => {
-            console.log(error.text);
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops, something went wrong',
-              text: error.text,
-            })
-          });
-        e.target.reset()
-      };
+  const [submitted, setSubmitted] = useState(false);
+  
+  function handleSubmit (e) {
+    e.preventDefault();
+    setSubmitted(true);
+  }
     return(
         <div className="contact" id="contact">
             <h1>Contact</h1>
-        <Form onSubmit={handleOnSubmit}>
-                <Form.Field
-                className='form-label'
-                id='form-input-control-email'
-                control={Input}
-                label='Email'
-                name='from_email'
-                placeholder='Email'
-                required
-                icon='mail'
-                iconPosition='left'
-                />
-                <Form.Field
-                id='form-input-control-last-name'
-                className='form-label'
-                control={Input}
-                label='Name'
-                name='from_name'
-                placeholder='Name'
-                required
-                icon='user circle'
-                iconPosition='left'
-                />
-                <Form.Field
-                id='form-textarea-control-opinion'
-                className='form-label'
-                control={TextArea}
-                label='Message'
-                name='message'
-                placeholder='Message'
-                required
-                />
-                <Button type='submit' color='green'>Submit</Button>
-          </Form>
+            {submitted && <div class='success-message'>Thanks! I'll get back to you ASAP.</div>}
+            <form
+            name="contact-form"
+            method="POST"
+            onSubmit={handleSubmit}
+            data-netlify="true">
+              <input type="text" placeholder="Enter your name" name="name" />
+              <input type="email" placeholder="Enter your email" name="email" />
+              <textarea type="text" placeholder="Type your message here" name="name" />
+              <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
